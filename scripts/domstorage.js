def27f9092c14c6333781876e54909681ddea0f9
Bridget Keehan <ba.keehan@gmail.com>
2018-05-09 16:14:44 -0400

    functionality: game checks guesses, updates points+view

--- scripts/domstorage.js
+++ scripts/domstorage.js
@@ -1,3 +1,7 @@
 var progressDisplay = document.getElementById("progress");
 
 var scoreDisplay = document.getElementById("score");
+
+
+var bankDisplay = document.getElementById("bank");
+var wrongDisplay = document.getElementById("wrong");
--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,6 @@
 var gameScore = 0;
 var progress = [];
+var current = [];
 var bank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wrong = [];
 
@@ -16,13 +17,39 @@ function stageGame (word, index) {
 	// shows PROGRESS in window
 	scoreDisplay.innerHTML = gameScore;
 	// shows initial SCORE in window
+	current = word.array;
+	// updates CURRENT array for testing
+	console.log(word.word);
+	// for testing and/or cheating
+}
+
+
+function testKey (letter, word) {
+	if (word.includes(letter)) {
+		for (i=0; i<word.length;i++) {
+			if (word[i] == letter) {
+				progress[i] = letter;
+			}
+		}
+		// updates PROGRESS to include correct guesses
+		progressDisplay.innerHTML = "";
+		for (i=0;i<progress.length;i++) {
+			progressDisplay.innerHTML += progress[i];
+		}
+		// updates PROGRESS in window
+	} else {
+		gameScore -= 1;
+		scoreDisplay.innerHTML = gameScore;
+		// updates GAMESCORE
+		wrong.push(letter);
+		wrongDisplay.innerHTML = wrong;
+		// updates and shows WRONG array
+	}
 }
 
 
 function playGame (index) {
-
 	stageGame(words[index], index);
-
 }
 
 playGame(Math.floor(Math.random()*words.length));
