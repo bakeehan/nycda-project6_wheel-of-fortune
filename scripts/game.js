0deca00fa6f1c9034a53eec2ced752eae325fce9
Bridget Keehan <ba.keehan@gmail.com>
2018-05-10 11:23:07 -0400

    started sass and gridded out basic layout

--- scripts/game.js
+++ scripts/game.js
@@ -74,6 +74,7 @@ function playGame (index) {
 		progressDisplay.innerHTML = "";
 		bank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 		wrong = [];
+		// clears stored TEMP INFO
 		stageGame(words[index], index);
 	} else if (gameScore > 5) {
 		gameDisplay.innerHTML = gameScore + " points! You win!";
