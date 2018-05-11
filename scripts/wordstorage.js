1bea9caf2df995f0ccb8d83333a3765640bd3f1d
Bridget Keehan <ba.keehan@gmail.com>
2018-05-11 12:11:04 -0400

    added more words

--- scripts/wordstorage.js
+++ scripts/wordstorage.js
@@ -13,6 +13,14 @@ function Word (word, hint, value) {
 
 var cerulean = new Word ("cerulean", "Like blue, but fancier.", 3);
 var popsicle = new Word ("popsicle", "A summer treat!", 1);
-var cryptozoology = new Word ("cryptozoology", "A strange field of study...", 5);
-var selkie = new Word ("selkie", "Is she a seal?", 4);
-var vim = new Word ("vim", "--and vigor!", 3);
+var cryptozoology = new Word ("cryptozoology", "A strange field of study...", 4);
+var selkie = new Word ("selkie", "Is she a seal?", 3);
+var vim = new Word ("vim", "--and vigor!", 5);
+var eldritch = new Word ("eldritch", "arcane and otherworldly", 4);
+var distinctive = new Word("distinctive", "characteristic", 2);
+var armadillo = new Word("armadillo", "What a cute little round suit of armor!", 3);
+var lollipop = new Word ("lollipop", "Candy on a stick", 1);
+var vaudeville = new Word ("vaudeville", "Old time variety shows",2);
+var again = new Word ("again", "Curses! Foiled _____!", 1);
+var beau = new Word ("beau", "A Hot Date", 4);
+var jazz = new Word ("jazz", "It's swingin'.", 5);
